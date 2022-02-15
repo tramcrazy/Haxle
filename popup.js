f5d9f668e3fd598074701922d4e9f5dfee2b7895
tramcrazy <81013186+tramcrazy@users.noreply.github.com>
2022-02-15 15:46:13 +0000

    Implement Bytle compatibility

--- popup.js
+++ popup.js
@@ -1,5 +1,9 @@
-function doHaxle() {
+function doHexHaxle() {
     return Number((69420*Math.floor(((new Date).getTime()-new Date("2022-01-24T00:00:00.000Z").getTime())/864e5)+80085)%65536).toString(16);
 }
+function doBinHaxle() {
+    return (((Math.floor((new Date().getTime() - new Date("2022-01-24T00:00:00.000Z").getTime()) / (24 * 60 * 60 * 1_000)) + 100) * 80085) + 69420) % 256;
+}
 
-document.getElementById("haxleCell").innerHTML = doHaxle();
+document.getElementById("haxleCell").innerHTML = doHexHaxle();
+document.getElementById("binHaxleCell").innerHTML = doBinHaxle();
